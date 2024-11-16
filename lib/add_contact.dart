08a55cd87c95baf90bbc3bfa3c96a7f7038e0173
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-11-16 00:05:46 -0300

    wip: add contact form

--- lib/add_contact.dart
+++ lib/add_contact.dart
@@ -16,7 +16,56 @@ class AddContact extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Capybara"),
       ),
+      body: const ContactForm(),
     );
     
+  }
+}
+
+class ContactForm extends StatefulWidget {
+  const ContactForm({super.key});
+
+  @override
+  State<StatefulWidget> createState() =>
+  ContactFormState();
+}
+
+class ContactFormState extends State<ContactForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Contact name',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          DatePickerDialog(firstDate: DateTime(2022), lastDate: DateTime(2024), helpText: "Select contact's birth date",),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState!.validate()) {
+                  // Process data.
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
